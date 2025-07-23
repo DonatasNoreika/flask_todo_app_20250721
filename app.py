7fc133cc67059be4c597902239a85eac26f4aab8
DonatasNoreika <donoras@gmail.com>
2025-07-23 19:44:13 +0300

    Slaptažodžio keitimas: sukurtas slaptažodžio pakeitimo puslapis

--- app.py
+++ app.py
@@ -71,6 +71,10 @@ class UzklausosAtnaujinimoForma(FlaskForm):
     el_pastas = StringField('El. paštas', [DataRequired(), Email()])
     submit = SubmitField('Įvesti')
 
+class SlaptazodzioAtnaujinimoForma(FlaskForm):
+    slaptazodis = PasswordField("Slaptažodis", validators=[DataRequired()])
+    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", validators=[DataRequired(), EqualTo("slaptazodis")])
+    submit = SubmitField('Įvesti')
 
 # modeliai
 class Vartotojas(db.Model, UserMixin):
@@ -83,6 +87,16 @@ class Vartotojas(db.Model, UserMixin):
     def get_reset_token(self):
         s = Serializer(app.config['SECRET_KEY'])
         return s.dumps({"user_id": self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return db.session.get(Vartotojas, user_id)
+
 
 class Uzduotis(db.Model):
     __tablename__ = "uzduotis"
@@ -189,9 +203,22 @@ def send_reset_email(user):
     # mail.send(msg)
 
 
-@app.route("/reset_password/<token>")
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
 def reset_token(token):
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = SlaptazodzioAtnaujinimoForma()
+    user = Vartotojas.verify_reset_token(token)
+    if user is None:
+        flash("Užklausa netinkama arba pasibaigusio galiojimo", 'warning')
+        return redirect(url_for("reset_request"))
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.slaptazodis.data).decode("utf-8")
+        user.slaptazodis = hashed_password
+        db.session.commit()
+        flash("Tavo slaptažodis buvo atnaujintas! Gali prisijungti", "success")
+        return redirect(url_for("prisijungti"))
+    return render_template("reset_token.html", form=form)
 
 
 @app.route("/reset_password", methods=['GET', 'POST'])
